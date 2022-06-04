d2030b33962524a882d5e5bd5a3830ac62a866fa
Elodin <jcassola96@gmail.com>
2022-06-04 17:02:24 -0400

    increase and decrease counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,12 @@
 <template>
   <h2>Counter</h2>
   <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
-  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
-  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
-  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
-  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
-  <p>{{counter}} <sup>2</sup> = {{squareCounter}}</p>
+
+  <div>
+      <button @click="decrease">-1</button>
+      <button @click="increase">+1</button>
+  </div>
+
 </template>
 
 <script>
@@ -18,13 +19,17 @@ export default {
     },
     methods: {
         getSquareValue(){
-            console.log('getSquareValue')
             return this.counter * this.counter
+        },
+        increase(){
+            this.counter++
+        },
+        decrease(){
+            this.counter--
         }
     },
     computed: {
         squareCounter(){
-            console.log('computed squareCounter')
             return this.counter * this.counter
         }
     }
@@ -33,5 +38,20 @@ export default {
 </script>
 
 <style>
+
+    button{
+        background-color: #64BB87;
+        border-radius: 5px;
+        border: 1px solid white;
+        color: white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+    }
+    button:hover{
+        background-color: #5aa67b;
+        transition: 0.3s ease-in-out;
+    }
 
 </style>
