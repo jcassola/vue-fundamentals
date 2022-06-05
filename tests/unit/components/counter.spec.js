1de34611a0d76057c6a100712461715431b2b8b5
Elodin <jcassola96@gmail.com>
2022-06-05 17:01:21 -0400

    h2 test

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -4,10 +4,21 @@ import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
     
-    test('debe de hacer match con el snapshot', () => {
+    // test('debe de hacer match con el snapshot', () => {
 
+    //     const wraper = shallowMount(Counter)
+
+    //     expect(wraper.html()).toMatchSnapshot()
+    // })
+
+    test('h2 debe de tener el valor por defecto "Counter" ', () => {
+        
         const wraper = shallowMount(Counter)
 
-        expect(wraper.html()).toMatchSnapshot()
-    })
+        expect(wraper.find('h2').exists()).toBeTruthy()
+
+        const h2Value = wraper.find('h2').text()
+
+        expect(h2Value).toBe('Counter')
+    });
 })
